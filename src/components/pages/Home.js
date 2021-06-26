5c944807ecc761fa5305acf81b6f793dbb6081fe
kaviyarasu <kaviviswa56@gmail.com>
2021-06-26 16:53:55 +0530

    added pages,material ui,router

---
+++ src/components/pages/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Home.css";
+
+import SearchIcon from "@material-ui/icons/Search";
+import AppsIcon from "@material-ui/icons/Apps";
+function Home() {
+  return (
+    <div className="home">
+      <h1>home</h1>
+      <div className="home__header">
+        <div className="header__left">
+          {/* Link */}
+          {/* Link */}
+        </div>
+        <div className="header__right">
+          {/* Link */}
+          {/* Link */}
+
+          {/* Link */}
+        </div>
+      </div>
+      <div className="home__body"></div>
+    </div>
+  );
+}
+
+export default Home;
